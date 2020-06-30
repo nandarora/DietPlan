394160199b82cd0e3f6c5a581be0e4b5b835ce2d
Nand Nandan Arora <nandnandanarora1@gmail.com>
2020-06-30 15:41:12 -0400

    commit

--- server.js
+++ server.js
@@ -8,12 +8,19 @@ const breakfast= require('./routes/api/breakfast');
 const lunch= require('./routes/api/lunch');
 const dinner= require('./routes/api/dinner');
 const userRoute= require('./routes/users');
+const path=require('path');
 
 mongoose.connect(db.mongodb)
 .then(function(){console.log('mongodb connected...')})
 .catch(function(err){console.log(err);})
 
+if(process.env.NODE_ENV === 'production'){
+    app.use(express.static('client/build'));
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
 
+}
 
 const app=express();
 app.use(cors());
